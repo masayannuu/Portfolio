8d12cc44364a68743d6d7ef2aa80e4433c670a06
masayannuu <masayannuu@gmail.com>
2018-03-26 23:50:24 +0900

    change path

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../../stylesheets/index.scss';
-import add from '../modules/addition-calculator';
-
-const number1 = 400;
-const number2 = 600;
-const total = add(number1, number2);
-
-console.log(total);
