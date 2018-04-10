0a25b75156a7d7abbee6ed9c1406803152368646
masayannuu <masayannuu@gmail.com>
2018-04-10 23:56:31 +0900

    add fontawesome

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import fontawesome from '@fortawesome/fontawesome'
+import faAngleRight from '@fortawesome/fontawesome-free-solid/faAngleRight'
 
 // Your top level component
 import App from './App'
 
 // Export your top level component as JSX (for static rendering)
 export default App
+
+// Add fontawesome
+fontawesome.library.add(faAngleRight)
 
 // Render your app
 if (typeof document !== 'undefined') {
