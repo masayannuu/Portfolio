b678ebef5cdd91cd83b63bdd89bead4dfe2bc424
masayannuu <masayannuu@gmail.com>
2018-05-16 00:04:19 +0900

    add page components

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,30 +1,6 @@
 import React from 'react'
-
 import LandingPageTemplate from '../../templates/LandingPage/LandingPage'
-import FirebaseClient from '../../../../lib/Domain/FirebaseClient'
 
-class LandingPage extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      contents: []
-    }
-  }
-
-  componentDidMount () {
-    this.getData()
-  }
-
-  async getData () {
-    const client = new FirebaseClient()
-    const contents = await client.find('work_contents')
-    this.setState({ contents })
-  }
-
-  render () {
-    console.log(this.state.contents)
-    return <LandingPageTemplate contents={this.state.contents} />
-  }
-}
+const LandingPage = () => <LandingPageTemplate />
 
 export default LandingPage
