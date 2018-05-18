36a2925c76c41de7f1966040bcc8bdbc562f5fe7
masayannuu <masayannuu@gmail.com>
2018-05-18 13:29:36 +0900

    fix blank height

--- src/components/atoms/Section/StyledSection.js
+++ src/components/atoms/Section/StyledSection.js
@@ -2,14 +2,15 @@ import styled from 'styled-components'
 import { Section } from 'reactbulma'
 
 const StyledSection = styled(Section)`
-  margin-bottom: 229px;
   @media screen and (max-width: 480px) {
     width: 100%;
+    margin-bottom: 80px;
   }
   @media screen and (min-width: 481px) {
     width: 90%;
     margin-left: auto;
     margin-right: auto;
+    margin-bottom: 229px;
   }
 `
 
