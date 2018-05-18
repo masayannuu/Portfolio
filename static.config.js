8cadfa7d33b3785877443292214562bf00405108
masayannuu <masayannuu@gmail.com>
2018-05-18 10:48:57 +0900

    add favicon

--- static.config.js
+++ static.config.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
 import { ServerStyleSheet } from 'styled-components'
-
+import ReactStaticFavicons from '@kuroku/react-static-favicons'
 import Typekit from 'react-typekit'
+import path from 'path'
+
+const reactStaticFavicons = new ReactStaticFavicons({
+  // string: directory where the image files are written
+  outputDir: path.join(__dirname, 'dist'),
+
+  // string: the source imag
+  inputFile: path.join(__dirname, 'public', 'favicon.ico'),
+
+  // object: the configuration passed directory to favicons
+  configuration: {
+    icons: {
+      favicons: true
+      // other favicons configuration
+    }
+  }
+})
 
 export default {
   preact: true,
   getSiteData: () => ({
     title: 'Masaya Morimoto'
   }),
-  renderToHtml: (render, Comp, meta) => {
+  renderToHtml: async (render, Comp, meta) => {
     const sheet = new ServerStyleSheet()
     const html = render(sheet.collectStyles(<Comp />))
+    meta.faviconsElements = await reactStaticFavicons.render()
     meta.styleTags = sheet.getStyleElement()
     return html
   },
@@ -19,19 +37,21 @@ export default {
       const {
         Html, Head, Body, children, renderMeta
       } = this.props
-
+      console.log(renderMeta.faviconsElements)
       return (
         <Html>
           <Head>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <Typekit kitId="lyf0hzv" />
-            {renderMeta.styleTags}
             <link
               rel="stylesheet"
               crossOrigin="true"
               href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"
             />
+            {renderMeta.styleTags}
+            {renderMeta.faviconsElements}
+            <title>Masaya Morimoto</title>
           </Head>
           <Body>{children}</Body>
         </Html>
