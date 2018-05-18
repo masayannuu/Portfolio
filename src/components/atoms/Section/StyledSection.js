3e7c6e0eef01c0f8ff15daf43f6861cdde6da58f
masayannuu <masayannuu@gmail.com>
2018-05-18 22:38:32 +0900

    add padding

--- src/components/atoms/Section/StyledSection.js
+++ src/components/atoms/Section/StyledSection.js
@@ -4,6 +4,7 @@ import { Section } from 'reactbulma'
 const StyledSection = styled(Section)`
   @media screen and (max-width: 480px) {
     width: 100%;
+    padding: 2em 1.5em;
     margin-bottom: 150px;
   }
   @media screen and (min-width: 481px) {
