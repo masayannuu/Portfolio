075e7a241a2871dc7831a5eb8e992fe9e0c03580
masayannuu <masayannuu@gmail.com>
2018-06-06 23:24:22 +0900

    add landingpates test

---
+++ test/src/components/templates/LandingPages/LandingPages.spec.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Enzyme from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+
+import LandingPage from '../../../../../src/components/templates/LandingPage/LandingPage'
+
+describe('LandingPage LandingPage Molecules', () => {
+  it('should render LandingPage', () => {
+    const wrapper = Enzyme.shallow(<LandingPage />)
+    expect(shallowToJson(wrapper)).toMatchSnapshot()
+  })
+})
