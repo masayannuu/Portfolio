8f8e2cbc228e0178e451880cd7da27ff2a22e6be
masayannuu <masayannuu@gmail.com>
2019-09-24 23:30:26 +0900

    update libs

--- static.config.js
+++ static.config.js
@@ -1,41 +1,24 @@
 import React, { Component } from 'react'
-import { ServerStyleSheet } from 'styled-components'
-import ReactStaticFavicons from '@kuroku/react-static-favicons'
 import Typekit from 'react-typekit'
 import path from 'path'
 
-const reactStaticFavicons = new ReactStaticFavicons({
-  // string: directory where the image files are written
-  outputDir: path.join(__dirname, 'dist'),
-
-  // string: the source imag
-  inputFile: path.join(__dirname, 'public', 'favicon.png'),
-
-  // object: the configuration passed directory to favicons
-  configuration: {
-    icons: {
-      favicons: true
-      // other favicons configuration
-    }
-  }
-})
-
 export default {
-  preact: true,
-  getSiteData: () => ({
-    title: 'Masaya Morimoto'
+  plugins: [
+    [
+      'react-static-plugin-favicons',
+      {
+        inputFile: path.resolve(__dirname, 'public', 'icon.svg'),
+      },
+      'react-static-plugin-styled-components',
+    ],
+  ],
+  getSiteData: async () => ({
+    title: 'Masaya Morimoto',
   }),
-  renderToHtml: async (render, Comp, meta) => {
-    const sheet = new ServerStyleSheet()
-    const html = render(sheet.collectStyles(<Comp />))
-    meta.faviconsElements = await reactStaticFavicons.render()
-    meta.styleTags = sheet.getStyleElement()
-    return html
-  },
   Document: class CustomHtml extends Component {
     render () {
       const {
-        Html, Head, Body, children, renderMeta
+        Html, Head, Body, children,
       } = this.props
       return (
         <Html>
@@ -48,13 +31,11 @@ export default {
               crossOrigin="true"
               href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"
             />
-            {renderMeta.styleTags}
-            {renderMeta.faviconsElements}
             <title>MASAYA MORIMOTO | PORTFOLIO</title>
           </Head>
           <Body className="has-navbar-fixed-top">{children}</Body>
         </Html>
       )
     }
-  }
+  },
 }
